d86a14045a5e0981d6a79f67dee2f317524f698c
11639583@qq.com <11639583@qq.com>
2019-05-07 16:14:31 +0800

    [refactor]: mongo lab connection

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,35 @@ const fs = require('fs');
 const path = require('path');
 const config = require('../config');
 const multer  = require('multer');
-const upload_path = path.join(path.join(path.dirname(__dirname), 'public'),'shapes');
-if (!fs.existsSync(upload_path)){
-    fs.mkdirSync(upload_path);
-}
-const upload = multer({ dest: upload_path });
+const shape_path = path.join(path.join(path.dirname(__dirname), 'public'),'shapes');
+const image_path = path.join(path.join(path.dirname(__dirname), 'public'),'images');
+if (!fs.existsSync(shape_path))  fs.mkdirSync(shape_path);
+if (!fs.existsSync(image_path))  fs.mkdirSync(image_path);
+const shape_upload = multer({ dest: shape_path });
+const image_upload = multer({ dest: image_path });
 /* GET home page. */
 router.get('/',  (req, res, next) => {
     res.render('index', { title: 'Green GIS Server', port: config.port || '4000' });
 });
 
-router.post('/upload', upload.array('file'),  (req, res) => {
+router.post('/upload/shape', shape_upload.array('file'),  (req, res) => {
     // move the file from the temporary location to the intended location
     req.files.forEach( file => {
         const origin_path = file.path;
-        const target_path = path.join(upload_path, file.originalname);
+        const target_path = path.join(shape_path, file.originalname);
+        fs.renameSync(origin_path, target_path);
+    });
+    res.status(200);
+    res.json({
+        result: true
+    });
+});
+
+router.post('/upload/image', image_upload.array('file'),  (req, res) => {
+    // move the file from the temporary location to the intended location
+    req.files.forEach( file => {
+        const origin_path = file.path;
+        const target_path = path.join(image_path, file.originalname);
         fs.renameSync(origin_path, target_path);
     });
     res.status(200);
